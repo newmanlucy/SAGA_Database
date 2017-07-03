def4cc31947e196be488e8ce3e74f0f646dad587
CYK <kongc@uchicago.edu>
2017-07-03 14:24:29 -0500

    put everything into folder + resolution count

---
+++ Convert_Videos/video_res_get.sh
@@ -0,0 +1,1 @@
+cat video_res.txt | sort | uniq -c | sort -nr > video_res_sorted.txt
---
+++ Convert_Videos/video_resolutions.sh
@@ -0,0 +1,6 @@
+while IFS='' read -r line || [[ -n "$line" ]]; do
+    echo "Text read from file: $line"
+    eval $(ffprobe -v error -of flat=s=_ -select_streams v:0 -show_entries stream=height,width "$line")
+    size=${streams_stream_0_width}x${streams_stream_0_height}
+    echo $size
+done < "$1"
