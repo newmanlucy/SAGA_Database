a239efa82e59d065ae8695c629034fa892446ded
CYK <kongc@uchicago.edu>
2017-07-13 15:03:16 -0500

    added some test files etc

---
+++ Convert_Videos/video_enc_get.sh
@@ -0,0 +1,3 @@
+while IFS='' read -r line || [[ -n "$line" ]]; do 
+    ffprobe -v error -select_streams v:0 -show_entries stream=codec_name -of default=nokey=1:noprint_wrappers=1 "$line"
+done < "$1"
