477b1c34e3305effd956065526ff9ef712747921
CYK <kongc@uchicago.edu>
2017-07-13 15:02:01 -0500

    use this one. works with settings.txt and list_files_here.txt

---
+++ Convert_Videos/exec_and_time_resolutions.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Variables to change
+WIDTH=640
+HEIGHT=480
+ASPRATIO=$(echo "$WIDTH/$HEIGHT" | bc -l)
+SETWIDTH="$WIDTH:-1"
+SETHEIGHT="-1:$HEIGHT"
+SETRATIO=$SETWIDTH
+
+# Setup
+LOG="${0}.log"
+
+if [ -f $LOG ] ; then
+    rm $LOG
+fi
+
+#Script
+while IFS='' read -r lineF; do
+    echo "File: ../${lineF}" >> $LOG
+    eval $(ffprobe -v error -of flat=s=_ -select_streams v:0 -show_entries stream=height,width "../$lineF")
+    if [ $(echo " $ASPRATIO > ${streams_stream_0_width}/${streams_stream_0_height}" | bc -l) = 0 ]; then
+        SETRATIO=$SETWIDTH
+    else
+        SETRATIO=$SETHEIGHT
+    fi
+    while IFS='' read -r lineS; do
+        echo "Settings: ${lineS}" >> $LOG
+        #echo "ffmpeg -i ../${lineF} ${lineS} -vf scale=$SETRATIO \"../webm_video/${lineF}_HEY.webm\" < /dev/null" >> $LOG
+        time -p "ffmpeg -i ../${lineF} ${lineS} -vf scale=$SETRATIO \"../webm_video/${lineF}_HEY.webm\" < /dev/null" 2>>$LOG
+        echo "" >> $LOG
+    done < "settings.txt"
+done < "list_files_here.txt"
+
+
+#done 3< "commands.txt" 4< "list_files_here.txt"
