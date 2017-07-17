fc04037d648f8089ae0a94768e00c88172b48207
Lucy Newman <newmanlucy@cs.uchicago.edu>
2017-07-17 11:14:26 -0500

    re-added video path files

--- Convert_Videos/video_res_get.sh
+++ Convert_Videos/video_res_get.sh
@@ -1,1 +1,10 @@
-cat video_res.txt | sort | uniq -c | sort -nr > video_res_sorted.txt
+{
+    while IFS='' read -r line; do
+#        echo "Text read from file: $line"
+        eval $(ffprobe -v error -of flat=s=_ -select_streams v:0 -show_entries stream=height,width "$line")
+        size=${streams_stream_0_width}x${streams_stream_0_height}
+        echo $size
+    done < "video_files.txt"
+} | sort | uniq -c | sort -nr > video_res_sorted.txt
+
+# while IFS='' read -r line || [[ -n "$line" ]]; do
