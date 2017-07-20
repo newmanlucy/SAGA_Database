7b494ce1b37fd34d1d9a3b2b033bf40cad0aa3a6
CYK <kongc@uchicago.edu>
2017-07-20 13:16:08 -0500

    fixed redirection to work on server

--- Convert_Videos/run_through_all_files.sh
+++ Convert_Videos/run_through_all_files.sh
@@ -6,14 +6,19 @@ ext=".mp4"
 
 # Setup
 LOG="${0}.log"
+LOGTIME="${0}_time.log"
 
 if [ -f $LOG ] ; then
     rm $LOG
 fi
 
+if [ -f $LOGTIME ] ; then
+    rm $LOGTIME
+fi
+
 #Script
 while IFS='' read -r line || [[ -n "$line" ]] ; do
-    echo "File: ${line}" >> $LOG
+    echo "File: ${line}" | tee $LOG $LOGTIME
     filename="$(echo "${line}" | rev | cut -d"/" -f1 | rev)"
     if [ "$codec_file" == "$codec" -a ${line: -4} == $ext ] ;
     then
@@ -23,7 +28,7 @@ while IFS='' read -r line || [[ -n "$line" ]] ; do
         script() {
             ffmpeg -i $line -f mp4 -c:v libx264 -c:a copy -crf 17 "$filename.h264.mp4" < /dev/null
         }
-        time -p script 2>>$LOG
-        echo "" >> $LOG
+        (time -p script) >>$LOG 2>>$LOGTIME
+        echo "" | tee $LOG $LOGTIME
     fi
 done < "video_files.txt"
