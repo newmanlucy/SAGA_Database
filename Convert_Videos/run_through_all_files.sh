43e623a747898872500c8d8ef16d3e4a75e374fb
CYK <kongc@uchicago.edu>
2017-07-20 11:35:09 -0500

    run_through_all_files for mp4 conversion

--- Convert_Videos/run_through_all_files.sh
+++ Convert_Videos/run_through_all_files.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
+
+# Variables to change
+codec="h264"
+ext=".mp4"
+
+# Setup
+LOG="${0}.log"
+
+if [ -f $LOG ] ; then
+    rm $LOG
+fi
+
+#Script
 while IFS='' read -r line || [[ -n "$line" ]] ; do
-    echo "File: ${line}"
-    ffprobe ${line}
-    ffmpeg -i $line -f webm -c:v libvpx-vp9 -c:a libvorbis -ss 00:01:00 -t 00:00:02 -vf scale=-1:480 -b:v 500k "${line}_avgbr_500k.webm" < /dev/null
+    echo "File: ${line}" >> $LOG
+    if [ "$codec_file" == "$codec" -a ${line: -4} == $ext ] ;
+    then
+        codec_file=$(ffprobe -v error -select_streams v:0 -show_entries stream=codec_name -of default=nokey=1:noprint_wrappers=1 test1.mov)
+        time -p "ffmpeg -i $line -f mp4 -c:v libx264 -c:a copy -crf 17 "${line}.h264.mp4" < /dev/null" 2>>$LOG
+        echo "" >> $LOG
+    else
+        echo "no"
+    fi
 done < "list_files_here.txt"
