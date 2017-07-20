3e80d16e40f610ca49560cae1aa1c0325cb760ef
CYK <kongc@uchicago.edu>
2017-07-20 11:52:08 -0500

    fixed if statement

--- Convert_Videos/run_through_all_files.sh
+++ Convert_Videos/run_through_all_files.sh
@@ -16,10 +16,10 @@ while IFS='' read -r line || [[ -n "$line" ]] ; do
     echo "File: ${line}" >> $LOG
     if [ "$codec_file" == "$codec" -a ${line: -4} == $ext ] ;
     then
+        cp $line "${line}.h264.mp4"
+    else
         codec_file=$(ffprobe -v error -select_streams v:0 -show_entries stream=codec_name -of default=nokey=1:noprint_wrappers=1 test1.mov)
         time -p "ffmpeg -i $line -f mp4 -c:v libx264 -c:a copy -crf 17 "${line}.h264.mp4" < /dev/null" 2>>$LOG
         echo "" >> $LOG
-    else
-        echo "no"
     fi
 done < "video_files.txt"
